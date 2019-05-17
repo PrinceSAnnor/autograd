b8186041c8a500b1d15e2fb6b3fc95a67ffb3124
Isaac Attuah <isaacattuah@gmail.com>
2019-05-17 10:59:03 -0700

    CSV manipulation

--- sheets/sheetPrint.py
+++ sheets/sheetPrint.py
@@ -1,11 +1,14 @@
 import gspread
-#import csv
+import csv
 from oauth2client.service_account import ServiceAccountCredentials
 
 #For the json file you'll need to share the client email provided with desired sheet file you want to edit
 #scope for spreadsheets and for drive
 scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive' ]
 credentials = ServiceAccountCredentials.from_json_keyfile_name('spreadsheet-example-240602-3df61217e12e.json', scope)
+
+
+
 
 gc = gspread.authorize(credentials)
 
@@ -15,3 +18,10 @@ wks = gc.open('SuaCode Test').sheet1
 
 #wks.append_row(['First Name','Last Name','Other Name', 'Met Deadline','Submitted','Original Total Score','Converted Score','Criteria Missed','Resubmitted' ])
 wks.append_row(['First Name','Last Name','Group Name', 'Grade','Error 1', 'Error 2', 'Error 3', 'Error 4', 'Error 5']);
+
+with open('results.csv') as csvfile:
+    readCSV = csv.reader(csvfile, delimiter=',')
+    for row in readCSV:
+        wks.append_row(row)
+        #print(row[0])
+        #print(row[0],row[1],row[2],)
