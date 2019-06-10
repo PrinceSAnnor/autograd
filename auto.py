7d5123dd1b5997be586207b1ed0d240e7a016de1
Samuel Boateng <kaybeta500@gmail.com>
2019-06-10 18:57:13 +0000

    code currently getting all course ids

--- auto.py
+++ auto.py
@@ -14,18 +14,20 @@ class AutoGrad(object):
 
     def __init__(self):
         """ Make initialisations here """
-
+        self.BASE_DIR = os.getcwd()
         self.classroom = None
         self.drive = None
         self.mailer = None
         self.sheet = None
         self.TURNED_IN = "TURNED_IN"
+        self.assg_name = ""
         self.attachments = []
-        self.course_names = (
-            ("SuaCode Africa 1"),
-            ("SuaCode Africa 2"),
-            ("SuaCode Africa 3"),
-        )
+        self.course_ids = []
+        self.course_names = {
+            "SuaCode Africa 1":"",
+            "SuaCode Africa 2":"",
+            "SuaCode Africa 3":"",
+        }
         self.assg_names = (
             ('Assignment 1 - Make Pong Interface'),
             ('Assignment 2 - Move ball'),
@@ -34,6 +36,7 @@ class AutoGrad(object):
             ('Assignment 5 - Add Extra Ball'),
             ('Assignment 6 - Add More Balls')
         )
+        self.sub_order = {'1': 0, '2': 1}
         self.boot()
     
     def boot(self):
@@ -41,14 +44,23 @@ class AutoGrad(object):
         
         # create a required instances
         self.teacher = Teacher()
-        # self.drive = Drive()
+        self.drive = Drive()
         # self.mailer = Mail()
         # self.sheet = Sheets()
 
 
-    def download_files(self, assignment_ids):
+    def download_files(self, file_info):
         """ Download necessary files to be graded """
-        
+        click.echo("Downloading assignment files ...")
+
+        file_path = 'assets/code/%s/' % (self.assg_name)
+        os.chdir(file_path)                                  
+        # try:
+        #     # self.drive.get_file(file_info['id'], file_name)
+        #     pass
+        # except:
+        #     pass
+
 
     def progress(self, i):
         """ TODO: remove this into an external file"""
@@ -57,10 +69,12 @@ class AutoGrad(object):
 
     def get_submissions_for_assignment(self, course_number, assignment_number):      
         """ Returns TURNED_IN submissions from one assignment of specified course/classroom(s) """
-
+        subs_list = []
+        
         def has_turned_in_and_has_state_history(s):
             return s['state'] == self.TURNED_IN and len(s['submissionHistory']) > 1
-           
+        
+       
         # type cast id
         assignment_number = int(assignment_number)
 
@@ -75,12 +89,18 @@ class AutoGrad(object):
 
         # Get course ids for selected courses eg. suacode 1 and 2 
         try:
-            course_ids = tuple(map(lambda cn: courses[self.course_names[ int(cn)-1]], course_number))
-        except:
-            click.echo("Could not get courses.\nMake sure you type a correct course number. Try 'autograd.py --help' for details")
+            for ck in courses.keys():
+                if ck in self.course_names:
+                    self.course_names[ck] = courses[ck]
+
+            # Keep ids for later
+            self.course_ids = self.course_names.values()
+            print(self.course_ids)
+        except Exception as e:
+            click.echo("Could not get courses. Make sure you type a correct course number.\nTry 'autograd.py --help' for details")
+            click.echo("Exception: {}".format(e))
         
-        
-        for id in course_ids:      
+        for id in self.course_ids:      
             # Get dict of assignment names and assignment id
             assignments = self.teacher.get_all_assignment_ids(id)
 
@@ -88,14 +108,15 @@ class AutoGrad(object):
                 # Get assignment id for selected assignment to be graded
                 assg_name = self.assg_names[assignment_number-1]
                 assg_id = assignments[ assg_name ]
-            except:
+                self.assg_name = assg_name 
+            except Exception as e:
                 click.echo("Could not get assignment {}".format(assignment_number))
-            #click.echo( assg_name +":"+assg_id)
+                raise e
 
-            subs = self.teacher.get_student_submissions(id,assg_id).get('studentSubmissions', [])
-            turned_in_subs = list(filter(lambda s: has_turned_in_and_has_state_history(s) ,subs))
+            subs_list += self.teacher.get_student_submissions(id,assg_id).get('studentSubmissions', [])
             
-            return turned_in_subs
+        turned_in_subs = list(filter(lambda s: has_turned_in_and_has_state_history(s), subs_list))        
+        return turned_in_subs
 
 
     def get_files_for_download(self, subs):
@@ -103,6 +124,7 @@ class AutoGrad(object):
 
         def getPDE(sub):
             userId = sub["userId"]
+            courseId = sub["courseId"]
             a = sub["assignmentSubmission"]["attachments"]
 
             if a:
@@ -113,6 +135,8 @@ class AutoGrad(object):
                     
                     if driveFile["title"][-4:] == ".pde":
                         final["userId"] = userId
+                        final["courseId"] = courseId
+                        final["id"] = driveFile["id"]
                         final["driveFile"] = driveFile["alternateLink"]
                         return final
             else:
@@ -121,7 +145,7 @@ class AutoGrad(object):
         attachments = list(map( lambda sub: getPDE(sub) ,subs))
         self.attachments = attachments # Attach to instance
         click.echo("Retrieved {} filename(s)".format(len(attachments)))
-        
+       
         return attachments
 
 
@@ -147,26 +171,53 @@ NO_OF_ASSIGNMENTS = 6
 NO_OF_ALLOWED_RESUBS = 2
 
 docs = {
-    "course_number": "Specify the course. 1 for Suacode Africa 1, 2 for Suacode Africa 2,etc. Additionally \
+    "test":"Run without uploading",
+    "course": "Specify the course. 1 for Suacode Africa 1, 2 for Suacode Africa 2,etc. Additionally \
         you can specify multiple courses. Eg. --course_number=1,2,3 to pull assignments for course 1, 2 and 3",
-    "assignment_number":"Specify the assignment you want to grade.",
+    "assignment":"Specify the assignment you want to grade.",
     "submission": "Specify whether you're grading first submissions or resubs. Eg. --submission=2",
 }
    
 
-@click.command()
+@click.group(invoke_without_command=True)
 @click.option('--submission', '-s', type=click.Choice( list(str(i) for i in range(1, NO_OF_ALLOWED_RESUBS+1)) ), default="1", help=docs["submission"])
-@click.option('--course', '-c', required="True", help=docs["course_number"])
-@click.option('--assignment', '-a', type=click.Choice( list(str(i) for i in range(1, NO_OF_ASSIGNMENTS+1)) ), required="True", help=docs["assignment_number"])
-def run_autograd(course_number, assignment_number, submission):
+@click.option('--course', '-c', required=True, help=docs["course"])
+@click.option('--assignment', '-a', type=click.Choice( list(str(i) for i in range(1, NO_OF_ASSIGNMENTS+1)) ), required="True", help=docs["assignment"])
+@click.pass_context
+def cli(context, course, assignment, submission):
+    context.ensure_object(dict) #
+
+    # Attach the inputs to context object
+    context.obj['course'] = course
+    context.obj['assignment'] = assignment
+    context.obj['submission'] = submission
+
+    if context.invoked_subcommand is None:
+        click.echo("[TEST] Running AutoGrad..")
         a = AutoGrad()
-        subs = a.get_submissions_for_assignment(course_number, assignment_number)
+        subs = a.get_submissions_for_assignment(course, assignment)
         at = a.get_files_for_download(subs)
         a.log_to_file(at)
+        # status = a.download_files(at)
+        
+
+
+
+@cli.command()
+@click.pass_context
+def deploy(context):
+    """Run complete process"""
+    course, assignment, submission = context.obj['AutoGrad'], context.obj['course'], context.obj['assignment'], context.obj['submission']
+    
+    click.echo("[DEPLOY] Running AutoGrad..")
+    # a = AutoGrad()
+    # subs = a.get_submissions_for_assignment(course, assignment)
+    # at = a.get_files_for_download(subs)
+    # a.log_to_file(at)
         
 
 if __name__ == "__main__":
-    run_autograd()
+    cli(obj={})
         
 
 
