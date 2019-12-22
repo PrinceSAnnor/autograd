55db61f3ae7c090ff3aeb980cbac2547a5882b99
Samuel Boateng <kaybeta500@gmail.com>
2019-12-22 19:50:13 +0000

    modified create_assignment.py

--- classroom/create_assignment.py
+++ classroom/create_assignment.py
@@ -1,24 +1,13 @@
 # Create all assignments
 
 from modules.Teacher import Teacher
-
 import json
 
-# Source of assignments
-FROM = ['SuaCode Africa 1']
-
-# Destination of assignments
-TO = ['Coding 2019']
-
-def log_to_file( info, file_name="temporary.json"):
-    try:
-        f = open(file_name, "w")
-        json.dump(info, f)
-        f.close()
-        return True
-    except:
-        return False
-
+# Constants
+FROM = 'SuaCode Africa 1' # Assignment template
+TO = []
+YES = "y"
+NO = "n"
 BODY = {
         "title": "",
         "description": "",
@@ -31,11 +20,10 @@ BODY = {
         "assigneeMode": "",
         "submissionModificationMode": "",
     }
-
 due_date = {
             'year': 2019,
-            'month': 7,
-            'day' : 20,
+            'month': 12,
+            'day' : 24,
         }
 due_time = {
         "hours": 23,
@@ -47,49 +35,73 @@ state = "DRAFT"     # Possible values, PUBLISHED, DRAFT, DELETED, COURSE_WORK_ST
 work_type = "ASSIGNMENT" # Possible values COURSE_WORK_TYPE_UNSPECIFIED, ASSIGNMENT, MULTIPLE_CHOICE_QUESTION, SHORT_ANSWER_QUESTION
 assignee_mode = "ALL_STUDENTS" # Possible values ASSIGNEE_MODE_UNSPECIFIED, ALL_STUDENTS, INDIVIDUAL_STUDENTS
 submission_modification_mode = "MODIFIABLE" # Possible values SUBMISSION_MODIFICATION_MODE_UNSPECIFIED, MODIFIABLE_UNTIL_TURNED_IN, MODIFIABLE
+response = YES
 
+# Functions
+def save_json( info, file_name="temporary.json"):
+    try:
+        with open(file_name, "w") as f:
+            json.dump(info, f)
+            return True
+    except:
+        return False
 
-if __name__ == '__main__':
-    # Login to Classroom
-    teacher = Teacher()
+def load_json(file_path):
+    with open(file_path, "r") as f:
+        return json.load(f)
+
+def print_list_names(courses={}):
+    names = list(courses.keys())
+    print("Which course are you creating assignments for? ")
+    for a,b in enumerate(names):
+        output = "{}: {}".format(a,b)
+        print(output)
     
-    # Get course info from Google Classroom
-    course_info = teacher.get_all_courses()
-    course_ids_from = list(map(lambda name: course_info.get(name,''), FROM))
-    course_ids_to = list(map(lambda name: course_info.get(name,''), TO))
-    
-    print("FROM:")
-    print(course_ids_from)
+def test_get_all_courses():
+    return load_json('courses.json')
 
-    print("TO:")
-    print(course_ids_to)
+def get_course_name(index, courses={}):
+    names = list(courses.keys())
+    return names[int(index)]
 
-    # Get all assignment info from Google Classroom
-    assignments_info = list(map(lambda id: teacher.get_all_assignments(id), course_ids_from))
-    assignments_info = assignments_info[0].get("courseWork",[])
-    log_to_file(assignments_info)
- 
-    # Get assignment info from json file
-    with open("classroom/temporary.json", "r") as f:
-        assignments_info = json.load(f)
+def get_course_id(index, courses={}):
+    ids = list(courses.values())
+    return ids[int(index)]
 
-    for c in assignments_info:
-        mats = c.get("materials",[])[0]
-        BODY["title"] = mats["driveFile"]["driveFile"]["title"]
-        BODY["materials"] = [mats]
-        BODY["state"] = state
-        BODY["dueDate"] = due_date
-        BODY["dueTime"] = due_time
-        BODY["maxPoints"] = max_points
-        BODY["workType"] = work_type
-        BODY["assigneeMode"] = assignee_mode
-        BODY["submissionModificationMode"] = submission_modification_mode
+teacher = Teacher()
+courses_info = teacher.get_all_courses() 
+# save_json(courses_info) # test method
+# courses_info = test_get_all_courses() # test method
 
-        # Create assignments
-        for id in course_ids_to:
-            print("========")
-            print("Creating {} in classroom with id {}".format(BODY["title"], id))          
-            print(BODY) 
-       
-            # teacher.create_assignment(id, BODY)
-    
+while response != NO:
+    print_list_names(courses_info)
+    index = input("Enter index of course: ")
+    chosen = get_course_name(index, courses_info) 
+    print("You chose {}\n".format(chosen))
+    TO.append(chosen)
+    response = input("Do you want to enter another course name? ({} for yes, {} for no) :".format(YES, NO))
+
+print("Chosen: ",TO)
+
+FROM_id = courses_info.get(FROM, '')
+assignments_info = teacher.get_all_assignments(FROM_id).get("courseWork", [])
+TO_ids = list(map(lambda name: courses_info.get(name) ,TO)) 
+
+for assignment in assignments_info:
+    mats = assignment.get("materials",[])[0]
+    BODY["title"] = mats["driveFile"]["driveFile"]["title"]
+    BODY["materials"] = [mats]
+    BODY["state"] = state
+    BODY["dueDate"] = due_date
+    BODY["dueTime"] = due_time
+    BODY["maxPoints"] = max_points
+    BODY["workType"] = work_type
+    BODY["assigneeMode"] = assignee_mode
+    BODY["submissionModificationMode"] = submission_modification_mode
+
+    # Create assignments
+    for id in TO_ids:
+        print("========")
+        print("Creating {} in classroom with id {}".format(BODY["title"], id))          
+        print(BODY) 
+        teacher.create_assignment(id, BODY)
